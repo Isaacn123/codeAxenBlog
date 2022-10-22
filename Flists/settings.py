0917f0ae931bfe8cd9b2755516bfd3a92d5db7fc
isaacn123 <nsambai72@gmail.com>
2022-10-22 16:39:16 +0300

    adding bb url

--- Flists/settings.py
+++ Flists/settings.py
@@ -95,7 +95,8 @@ db_config = dj_database_url.config(default=os.environ.get('CLEARDB_DATABASE_URL'
 
 if not DEBUG: 
     DATABASES = {'default': {}}
-    DATABASES['default'] = dj_database_url.config(default=os.environ.get('CLEARDB_DATABASE_URL'),conn_max_age=600)
+    prod_db = dj_database_url.config(env="CLEARDB_DATABASE_URL",conn_max_age=600)
+    DATABASES['default'].update(prod_db)
     
     # DATABASES['default'].update(prod_db)
 #   DATABASES = {
