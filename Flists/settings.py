2267e6e06c85b5cb80cf9e7d33bac09012517d4f
isaacn123 <nsambai72@gmail.com>
2022-11-17 22:13:32 +0300

    adding allowed hosts

--- Flists/settings.py
+++ Flists/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = str(os.environ.get('DEBUG')) == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 if not DEBUG:
     ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 
